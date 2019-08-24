d7db12100984aa223d0ad686170459573a9b0c02
Milena Kari <milenakari@yahoo.com>
2019-08-24 22:39:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,8 +6,14 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
-const input = document.querySelector('input');
- 
-input.addEventListener('keydown', function(e) {
-  console.log(e.which);
+
+//Prevent default behavior, e.g. receiving key 'g'
+
+input.addEventListener('keydown', function(e){
+  if (e.which === 71) {
+    console.log('Default prevened');
+    return e.preventDefault()
+  } else {
+    console.log('Not a "g"')
+  }
 });
